c7fb1f2044175b4c21f87124257a2cb9ec064763
hoylemd <hoylemd@gmail.com>
2012-12-06 02:09:45 +0000

    shortened it like a NIIINNNJJJJAAAAAA

--- chbs.py
+++ chbs.py
@@ -1,22 +1,13 @@
 import random
 from math import floor
 
-# load the wordlist
-list_file = open('/usr/share/dict/words', 'r')
-word_list = []
-word = list_file.readline()
-word_count = 1
-while (word != ""):
-  word_list.append(word)
-  word_count += 1
-  word = list_file.readline()
+word_list = open('/usr/share/dict/words', 'r').readlines()
 
 password = ""
 
 for i in range(0, 4):
   word = random.choice(word_list).replace("\n", "").lower()
   password += word[0:1].upper() + word[1:]
-  
 
 print password
 

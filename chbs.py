61b2f8f027c2eab72f900aaa51504c6ded217e68
hoylemd <hoylemd@gmail.com>
2012-12-06 02:52:32 +0000

    omg so tiny

--- chbs.py
+++ chbs.py
@@ -1,4 +1,2 @@
-import random, sys
-l=open('/usr/share/dict/words','r').readlines()
-for i in range(0,4): sys.stdout.write(random.choice(l).replace("\n", "").title())
-sys.stdout.write("\n")
+import random
+print ''.join([random.choice(open('/usr/share/dict/words','r').readlines()).strip().title() for i in range(4)])

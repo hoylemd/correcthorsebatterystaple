c622fc711f3534bccc09eb74b37dd001fe21990e
hoylemd <hoylemd@gmail.com>
2012-12-06 02:11:56 +0000

    even shorter. like a... short thing. I don't know i'm bad at this

--- chbs.py
+++ chbs.py
@@ -1,13 +1,8 @@
 import random
-from math import floor
-
 word_list = open('/usr/share/dict/words', 'r').readlines()
-
 password = ""
-
 for i in range(0, 4):
   word = random.choice(word_list).replace("\n", "").lower()
   password += word[0:1].upper() + word[1:]
-
 print password
 

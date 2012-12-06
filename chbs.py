f95effaca376dbad7dcdd204101f5148b97c4781
hoylemd <hoylemd@gmail.com>
2012-12-06 03:12:37 +0000

    made it pretty again

--- chbs.py
+++ chbs.py
@@ -1,2 +1,11 @@
 import random
-print ''.join([random.choice(open('/usr/share/dict/words','r').readlines()).strip().title() for i in range(4)])
+import contextlib
+
+with contextlib.closing(open('/usr/share/dict/words', 'r')) as f:
+	word_list = f.readlines()
+
+password = ""
+for i in range(4):
+	password += random.choice(word_list).strip().title()
+
+print password	

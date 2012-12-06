20ca07f7b7cd077be346a17263912cc760ddc8d1
hoylemd <hoylemd@gmail.com>
2012-12-06 02:37:59 +0000

    tiiiiny

--- chbs.py
+++ chbs.py
@@ -1,4 +1,4 @@
 import random, sys
 l=open('/usr/share/dict/words','r').readlines()
-for i in range(0,4):
-  sys.stdout.write(random.choice(l).replace("\n", "").title())
+for i in range(0,4): sys.stdout.write(random.choice(l).replace("\n", "").title())
+sys.stdout.write("\n")

aceb85644146b02333701d25f9971956b8634e45
hoylemd <hoylemd@gmail.com>
2013-01-02 23:21:28 -0500

    Added instruction comment

--- chbs.py
+++ chbs.py
@@ -1,3 +1,14 @@
+#!/bin/python
+
+#correcthorsebatterystaple
+#=========================
+
+#password generator
+
+#uses the wordlist found at /usr/share/dict/words
+
+#generates a camel-cased password of 4 random words from that list
+
 import random
 import contextlib
 
@@ -8,4 +19,4 @@ password = ""
 for i in range(4):
 	password += random.choice(word_list).strip().title()
 
-print password	
+print password
